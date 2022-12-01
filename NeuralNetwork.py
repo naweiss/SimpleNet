df7efe1f04f5b899fac0d93ecfa91ff7b4658845
naweiss <naweiss@g.jct.ac.il>
2022-12-02 00:45:18 +0200

    Simplify NeuralNetwork code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,40 +2,50 @@ from Matrix import Matrix
 import random
 import math
 
+
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
-def dsigmoid(x):
-    return sigmoid(x)*(1-sigmoid(x))
 
-class NeuralNetwork():
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+class NeuralNetwork:
     def __init__(self, sizes):
         self.sizes = sizes
         self.weights = []
         self.biases = []
-        for i in range(1,len(sizes)):
-            data = NeuralNetwork.__random_data(sizes[i],sizes[i-1])
-            self.weights.append(Matrix(sizes[i], sizes[i-1], data))
-            data = [[1]]*sizes[i]
-            self.biases.append(Matrix(sizes[i],1,data))
+        for i in range(1, len(sizes)):
+            self.weights.append(Matrix(
+                self.__random_data(n=sizes[i], m=sizes[i-1])
+            ))
+            # All ones
+            self.biases.append(Matrix(
+                [[1]] * sizes[i]
+            ))
 
+    @staticmethod
     def __random_data(n, m):
-        width = 1/math.sqrt(n)
+        width = 1 / math.sqrt(n)
         return [
-            [random.uniform(-width,width) for i in range(m)]
+            [random.uniform(-width, width) for i in range(m)]
             for j in range(n)
         ]
 
-    def feedforword(self, inputs):
-        if len(inputs) == self.sizes[0]:
-            output = Matrix(1,len(inputs), [inputs]).transpose()
-            for i in range(len(self.sizes)-1):
-                output = self.weights[i]*output+self.biases[i]
-                output = output.apply(sigmoid)
-            return output
-        raise Exception("Invalid input length")
+    def feedforword(self, input_data):
+        if len(input_data) != self.sizes[0]:
+            raise Exception("Invalid input length")
+
+        input_data = Matrix([input_data])
+
+        output = input_data.transpose()
+        for i in range(len(self.sizes) - 1):
+            output = (self.weights[i] * output) + self.biases[i]
+            output = output.apply(sigmoid)
+        return output
         
-    def backpropagation(self, x, y):
+    def backpropagation(self, input_data, output):
         """
         delta_new:
 
@@ -43,66 +53,77 @@ class NeuralNetwork():
                 *
                 delta
             **
-                output.apply(dsigmoid)
+                output.apply(sigmoid_derivative)
         *
-            sigmoided.transpose()
+            sigmoided_output.transpose()
         """
-        output = x
-        layers = [x]
+        layer_output = input_data
+        layers = [input_data]
         for weight, bias in zip(self.weights,self.biases):
-            output = weight*output+bias
-            layers.append(output)
-            output = output.apply(sigmoid)
-            layers.append(output)
+            layer_output = (weight * layer_output) + bias
+            layers.append(layer_output)
+            layer_output = layer_output.apply(sigmoid)
+            layers.append(layer_output)
 
-        delta_w, delta_b  = [], []
-        sigmoided, output  = layers.pop(), layers.pop()
-        delta = (sigmoided-y)**(output.apply(dsigmoid))
+        delta_weight, delta_bias  = [], []
+        sigmoided_output, layer_output  = layers.pop(), layers.pop()
+        delta = (sigmoided_output - output) ** layer_output.apply(sigmoid_derivative)
         
         for weight in reversed(self.weights):
-            sigmoided = layers.pop()
-            delta_b.insert(0,delta)
-            delta_w.insert(0,delta*(sigmoided.transpose()))
+            sigmoided_output = layers.pop()
+            delta_bias.insert(0, delta)
+            delta_weight.insert(0, delta * sigmoided_output.transpose())
             if layers:
-                output = layers.pop()
-                delta = (weight.transpose()*delta)**(output.apply(dsigmoid))
-        return (delta_w,delta_b)
+                layer_output = layers.pop()
+                delta = (weight.transpose() * delta) ** layer_output.apply(sigmoid_derivative)
+        return (delta_weight, delta_bias)
 
-    def update_mini_batch(self, batch, lr):
-        mean_w = [w.apply(lambda x: 0) for w in self.weights]
-        mean_b = [b.apply(lambda x: 0) for b in self.biases]
+    def update_mini_batch(self, batch, learning_rate):
+        average_weights = [weight.apply(lambda x: 0) for weight in self.weights]
+        average_bias = [bais.apply(lambda x: 0) for bais in self.biases]
 
         # summing all the deltas
-        for x, y in batch:
-            x, y = Matrix(1,len(x), [x]).transpose(), Matrix(1,len(y), [y]).transpose()
-            delta_w, delta_b = self.backpropagation(x, y)
-            for i in range(len(self.sizes)-1):
-                mean_w[i] += delta_w[i]
-                mean_b[i] += delta_b[i]
+        for input_data, output in batch:
+            input_data = Matrix([input_data]).transpose()
+            output = Matrix([output]).transpose()
+            delta_weight, delta_bias = self.backpropagation(input_data, output)
+            for i in range(len(self.sizes) - 1):
+                average_weights[i] += delta_weight[i]
+                average_bias[i]    += delta_bias[i]
 
         # averaging all the deltas
-        for i in range(len(self.sizes)-1):
-            mean_w[i] = mean_w[i].apply(lambda x: lr*x/len(batch))
-            mean_b[i] = mean_b[i].apply(lambda x: lr*x/len(batch))
+        for i in range(len(self.sizes) - 1):
+            average_weights[i] = average_weights[i].apply(lambda x: (learning_rate * x) / len(batch))
+            average_bias[i]    = average_bias[i].apply(lambda x: (learning_rate * x) / len(batch))
     
         # subtract the estimated deltas
-        for i, w, b in zip(range(len(self.sizes)),mean_w, mean_b):
-            self.weights[i] -= w
-            self.biases[i]  -= b
+        for i, weight, bais in zip(range(len(self.sizes)), average_weights, average_bias):
+            self.weights[i] -= weight
+            self.biases[i]  -= bais
                     
-    def train(self, data, lr, epochs, mini_batch_size=0, verbose=False):
-        if mini_batch_size < 1:
-            mini_batch_size = len(data)
+    def train(self, inputs, outputs, learning_rate=0.5, epochs=100, mini_batch_size=None, verbose=False):
+        if not mini_batch_size:
+            mini_batch_size = len(inputs)
+
+        data = list(zip(inputs, outputs))
         for i in range(epochs):
             random.shuffle(data)
             mini_batches = [
-                data[k:k+mini_batch_size]
-                for k in range(0, len(data), mini_batch_size)
+                data[batch_start_index: batch_start_index + mini_batch_size]
+                for batch_start_index in range(0, len(data), mini_batch_size)
             ]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, lr)
+                self.update_mini_batch(mini_batch, learning_rate)
             if verbose:
-                print("Epoch:",i+1)
+                print("Epoch:", i + 1)
+
+
+    def evaluate(self, inputs, outputs):
+        for input_data, output in zip(inputs, outputs):
+            training_output  = self.feedforword(input_data)
+            max_output_index = training_output.data.index(max(training_output.data))
+            expected_index   = output.index(max(output))
+            print('got output index: {}, expected output index: {}'.format(max_output_index, expected_index))
 
 if __name__ == "__main__":
     inputs  = [
@@ -117,22 +138,12 @@ if __name__ == "__main__":
         [0,1],
         [0,1]
     ]
-
-    nn   = NeuralNetwork([2,4,2])
+    xor_network = NeuralNetwork([len(inputs[0]), 4, len(outputs[0])])
     
     print("Not traind")
-    for j in range(len(inputs)):
-        output   = nn.feedforword(inputs[j])
-        got      = output.data.index(max(output.data))
-        expected = outputs[j].index(max(outputs[j]))
-        print(got,"<=>",expected)
-    
-    batch = list(zip(inputs, outputs))
-    nn.train(batch, lr=0.8, epochs=5000, mini_batch_size=4)
+    xor_network.evaluate(inputs, outputs)
+
+    xor_network.train(inputs, outputs, learning_rate=0.8, epochs=5000, mini_batch_size=4)
     
     print("Traind")
-    for j in range(len(inputs)):
-        output   = nn.feedforword(inputs[j])
-        got      = output.data.index(max(output.data))
-        expected = outputs[j].index(max(outputs[j]))
-        print(got,"<=>",expected)
+    xor_network.evaluate(inputs, outputs)
